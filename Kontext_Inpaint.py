912af4c6c1e8172ff97a3de60dd4662bdc6f2661
nypox <hello@nypox.com>
2025-09-03 07:11:55 +0000

    auto update

--- Kontext_Inpaint.py
+++ Kontext_Inpaint.py
@@ -16,7 +16,7 @@ class FluxKontextInpaintingConditioning:
                              }}
 
     RETURN_TYPES = ("CONDITIONING","LATENT")
-    RETURN_NAMES = ("conditioning", "latent")
+    RETURN_NAMES = ("conditioning", "latent_image")
     FUNCTION = "encode"
 
     CATEGORY = "conditioning/inpaint"
@@ -66,46 +66,11 @@ class FluxKontextInpaintingConditioning:
 
         return (conditioning, out_latent)
 
-    def encdasdsaode(self, positive, negative, pixels, vae, mask, noise_mask=True):
-        x = (pixels.shape[1] // 8) * 8
-        y = (pixels.shape[2] // 8) * 8
-        mask = torch.nn.functional.interpolate(mask.reshape((-1, 1, mask.shape[-2], mask.shape[-1])), size=(pixels.shape[1], pixels.shape[2]), mode="bilinear")
-
-        orig_pixels = pixels
-        pixels = orig_pixels.clone()
-        if pixels.shape[1] != x or pixels.shape[2] != y:
-            x_offset = (pixels.shape[1] % 8) // 2
-            y_offset = (pixels.shape[2] % 8) // 2
-            pixels = pixels[:,x_offset:x + x_offset, y_offset:y + y_offset,:]
-            mask = mask[:,:,x_offset:x + x_offset, y_offset:y + y_offset]
-
-        m = (1.0 - mask.round()).squeeze(1)
-        for i in range(3):
-            pixels[:,:,:,i] -= 0.5
-            pixels[:,:,:,i] *= m
-            pixels[:,:,:,i] += 0.5
-        concat_latent = vae.encode(pixels)
-        orig_latent = vae.encode(orig_pixels)
-
-        out_latent = {}
-
-        out_latent["samples"] = orig_latent
-        if noise_mask:
-            out_latent["noise_mask"] = mask
-
-        out = []
-        for conditioning in [positive, negative]:
-            c = node_helpers.conditioning_set_values(conditioning, {"concat_latent_image": concat_latent,
-                                                                    "concat_mask": mask})
-            out.append(c)
-        return (out[0], out[1], out_latent)
-
-
 
 NODE_CLASS_MAPPINGS = {
-    "Kontext Inpainting Conditioning": FluxKontextInpaintingConditioning,
+    "KontextInpaintingConditioning": FluxKontextInpaintingConditioning,
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = { 
-    "Kontext Inpainting Conditioning": "Kontext Inpainting Conditioning",
+    "KontextInpaintingConditioning": "KontextInpaintingConditioning",
 }
